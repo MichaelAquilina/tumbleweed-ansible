8015e4fcf2d34460e3d0f67bbd56428fdca1bff1
Michael Aquilina <michael.aquilina@snyk.io>
2020-04-02 16:17:13 +0100

    [sway] Add ability to take screenshot of currently selected window

--- roles/sway/files/bin/grim_wrapper.sh
+++ roles/sway/files/bin/grim_wrapper.sh
@@ -5,9 +5,21 @@ set -e
 
 location="$HOME/Pictures/$(date "+%Y-%m-%d_%H-%M-%S").png"
 
+# Take a screenshot with clipper tool (slurp)
 if [[ "$1" == "-g" ]]; then
     dimensions="$(slurp -d)"
     grim -g "$dimensions" "$location"
+
+# Take a screenshot of the current window
+elif [[ "$1" == "-w" ]]; then
+    rect="$(swaymsg -t get_tree | jq '.. | select(.focused?) | .rect')"
+    x="$(jq '.x' <<< "$rect")"
+    y="$(jq '.y' <<< "$rect")"
+    width="$(jq '.width' <<< "$rect")"
+    height="$(jq '.height' <<< "$rect")"
+    grim -g "${x},${y} ${width}x${height}" "$location"
+
+# Take a screenshot of the entire screen
 else
     grim "$location"
 fi
