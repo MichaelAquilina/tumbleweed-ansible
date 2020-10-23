a25b739b6ec84336ec500ad3ad07c82e4852c0cf
Michael Aquilina <michaelaquilina@gmail.com>
2020-10-23 10:21:53 +0100

    [waybar] Don't display empty artist/album in playerbar.sh script

--- roles/sway/files/bin/playerbar.sh
+++ roles/sway/files/bin/playerbar.sh
@@ -1,12 +1,23 @@
 #! /usr/bin/bash
 # Display appropriate metadata on waybar
 
-output="$(playerctl metadata -f '[{{status}}]  <b>{{title}}</b> by <i>{{artist}}</i> from <i>{{album}}</i>')"
+artist="$(playerctl metadata -f "{{artist}}")"
+album="$(playerctl metadata -f "{{album}}")"
+
+output="$(playerctl metadata -f '[{{status}}]  <b>{{title}}</b>')"
 output="$(printf "$output" | sed 's/\[Playing\]//')"
 output="$(printf "$output" | sed 's/\[Paused\]//')"
 
 # Replace & with &amp; to prevent markdown errors
 output="$(printf "$output" | sed 's/\&/\&amp;/')"
+
+if [[ -n "$artist" ]]; then
+    output="$output by <i>$artist</i>"
+fi
+
+if [[ -n "$album" ]]; then
+    output="$output from <i>$album</i>"
+fi
 
 if [[ "$output" != "[Stopped]"* ]]; then
     printf "$output"
