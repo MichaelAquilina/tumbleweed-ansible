bc2eff43eaa1fd8d7240973a02802c5b5750e3a5
Michael Aquilina <michaelaquilina@gmail.com>
2022-01-17 09:52:15 +0000

    [nvim] no need to independently configure jedi-language-server

--- roles/nvim/files/init.lua
+++ roles/nvim/files/init.lua
@@ -120,8 +120,6 @@ local lspconfig = require('lspconfig')
 local lsp_installer = require('nvim-lsp-installer')
 lsp_installer.on_server_ready(function (server) server:setup { capabilities = capabilities } end)
 
-lspconfig['jedi_language_server'].setup{ capabilities = capabilities }
-
 local saga = require('lspsaga')
 saga.init_lsp_saga({
   code_action_prompt = {enable = false}
